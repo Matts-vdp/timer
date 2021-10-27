b8c7cc34f1fad7707e7a45fe641968e9f79fd5a6
Matts <vdpmatts@gmail.com>
2021-10-27 21:48:22 +0200

    add functionality

--- timer.go
+++ timer.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"os"
+	"time"
 
 	"github.com/Matts-vdp/terminal/ter"
 )
@@ -12,4 +14,15 @@ func main() {
 	defer term.Close()
 	term.UpdateLine(0, "Start")
 	term.UpdateLine(1, "time passed")
+	now := time.Now()
+	for {
+		pas := time.Since(now)
+		term.UpdateLine(0, now.Format("15:04:05"))
+		hr := int(pas.Hours())
+		mn := int(pas.Minutes()) - int(pas.Hours())*60
+		sc := int(pas.Seconds()) - int(pas.Minutes())*60
+		s := fmt.Sprintf("%.2d:%.2d:%.2d", hr, mn, sc)
+		term.UpdateLine(1, s)
+		<-time.After(time.Second / 5)
+	}
 }
